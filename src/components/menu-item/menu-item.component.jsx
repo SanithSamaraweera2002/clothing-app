10e6ef1d8d4bb5a970900621e6a640656564b0c5
White Tangent <63760473+SanithSamaraweera2002@users.noreply.github.com>
2023-02-19 10:59:06 +0530

    Changes Done

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-const MenuItem = () => {
+import './menu-item.styles.scss';
+
+const MenuItem = ({title}) => (
     <div className='menu-item'>
         <div className='content'>
-            <h1 className='title'> HATS </h1>
+            <h1 className='title'> {title} </h1>
             <span className='subtitle'> SHOP NOW </span>
 
         </div>
     </div>
-}
+)
+
+export default MenuItem;
