33da63cef7124aa99d85ee6875315eb1e7012dad
White Tangent <63760473+SanithSamaraweera2002@users.noreply.github.com>
2023-02-19 11:47:46 +0530

    Homepage divided into component, styling of menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,10 +3,11 @@ import React from "react";
 import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size}) => (
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} 
+    <div 
     className={`${size} menu-item`}>
+        <div className='background-image' style={{
+        backgroundImage: `url(${imageUrl})`
+    }}  />
         <div className='content'>
             <h1 className='title'> {title.toUpperCase()} </h1>
             <span className='subtitle'> SHOP NOW </span>
